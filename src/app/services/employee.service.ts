36fd75599cce98d925c75eb0f3225e5c4efdcab4
Oscar <Oscar.zambrano@ionix.cl>
2024-09-03 11:53:48 +0200

    add configuration services url environments

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from '../employee/models/employee';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private apiUrl = 'https://your-backend-api.com/api/employee';
+  private apiUrl = environment.apiUrl + '/employees';
 
   constructor(private http: HttpClient) {}
 
   uploadEmployeeData(formData: FormData): Observable<any> {
+    console.log(this.apiUrl, 'URL')
     return this.http.post(`${this.apiUrl}`, formData);
   }
 
